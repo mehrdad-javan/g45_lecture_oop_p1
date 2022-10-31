77ee930fbd4553df990dec51e2bfcaac582c445e
Mehrdad Javan <mehrdad.javan@adapta.se>
2022-10-31 10:20:22 +0100

    oop part1 -> objects and classes

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,13 +1,28 @@
 package se.lexicon;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+
+import java.io.PrintStream;
+
+public class App {
+  public static void main(String[] args) {
+
+    Person erik = new Person("Erik", "Svensson", 42, false);
+    //erik.setFirstName("Erik");
+    //erik.setLastName("Svensson");
+    //erik.setAge(42);
+
+    //System.out.println("Person: " + erik.getFirstName() + " " + erik.getLastName());
+    System.out.println(erik.personInformation());
+
+    Person mehrdad = new Person("Mehrdad", "Javan", 33);
+
+    Person test = new Person("Test", "Test");
+
+
+
+
+
+
+
+  }
 }
---
+++ src/main/java/se/lexicon/Person.java
@@ -0,0 +1,75 @@
+package se.lexicon;
+
+public class Person {
+
+  // Fields
+  public String firstName; // null
+  private String lastName; // null
+  private int age; // 0
+  private boolean sleeping; // false
+
+  public Person() {
+    this.sleeping = false;
+  }
+
+  public Person(String firstName, String lastName, int age) {
+    this(firstName, lastName);
+    setAge(age);
+  }
+
+  public Person(String firstName, String lastName) {
+    this();
+    setFirstName(firstName);
+    setLastName(lastName);
+  }
+
+
+  public Person(String firstName, String lastName, int age, boolean sleeping) {
+    this(firstName, lastName);
+    setAge(age);
+  }
+
+
+  // Methods
+
+  // Getters & Setters
+  public void setFirstName(String firstName) {
+    if (firstName == null) throw new IllegalArgumentException("firstName param was null");
+    this.firstName = firstName;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setLastName(String lastName) {
+    if (lastName == null) throw new IllegalArgumentException("lastName param was null");
+    this.lastName = lastName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setSleeping(boolean sleeping) {
+    this.sleeping = sleeping;
+  }
+
+  public boolean isSleeping() {
+    return sleeping;
+  }
+
+  public String personInformation() {
+    return " Person: FirstName: " + firstName + " LastName: " + lastName + " Age: " + age;
+  }
+
+
+}
